cec2628f1a2e82862fbcdbf1399b3ec20bc173d4
saiteja218 <saitejareddy218@gmail.com>
2024-08-14 11:48:55 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import hospital from "./assets/hospital.png"
-import { Route,Routes } from 'react-router-dom';
+import { Navigate, Route,Routes, useNavigate } from 'react-router-dom';
 import Hospitals from './components/Hospitals';
 import Details from './components/Details';
 import Container from '@mui/material/Container';
@@ -8,13 +8,14 @@ import Container from '@mui/material/Container';
 
 
 export default function App() {
+    const navigate=useNavigate();
     return (
         <div >
             <div style={{borderBottom:"1px solid black"}}>
                 <Container>
                 <div style={{display:"flex",alignItems:"center",}}>
                    <img src={hospital} alt="hospital-log"  height={50} width={50}/>
-                   <h2>Med-Route</h2>
+                   <h2 style={{cursor:"pointer"}} onClick={()=>navigate("/")}>Med-Route</h2>
                    
                 </div>
                 </Container>
